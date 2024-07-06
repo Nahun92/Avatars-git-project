49dd7561b6a4e2d957efc59614a9346401d858c2
Nahun92 <andrade_nahun@yahoo.com>
2024-07-06 12:15:04 -0400

    my third push

--- src/test/java/org/example/testing.java
+++ src/test/java/org/example/testing.java
@@ -8,5 +8,9 @@ public class testing {
 
         System.out.println("My third git class");
 
+        System.out.println("I am having the time of my life");
+
+        System.out.println("cant wait to get a job");
+
     }
 }
